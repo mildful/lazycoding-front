475379d97cc2e54cafef3aac8f23fae68c635d63
Clement Flodrops <clement.flodrops@gmail.com>
2016-12-14 16:24:03 +0100

    add (fake) background animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Http, Response } from '@angular/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { AppState } from './reducers/index';
@@ -10,20 +18,36 @@ import { MOBILE } from './services/constants';
   selector: 'lc-app',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('bgImage', [
+      state('hidden', style({ opacity: 0 })),
+      state('show', style({ opacity: 1 })),
+      transition('hidden => show', [
+        // style({ opacity: 1 }),
+        animate('600ms ease-in-out')
+      ])
+    ])
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // utils
   showMonitor = (ENV === 'development' && !AOT &&
     ['monitor', 'both'].includes(STORE_DEV_TOOLS) // set in constants.js file in project root
   );
   contentWidth: string = this.showMonitor ? '70%' : '';
   mobile = MOBILE;
+  // other
+  bgImageAnimationState: string = 'hidden';
+  imageData: any;
 
   constructor(
     public route: ActivatedRoute,
     public router: Router,
     private store: Store<AppState>,
-    private categoryActions: CategoryActions
+    private categoryActions: CategoryActions,
+    private http: Http,
+    private sanitizer: DomSanitizer
   ) {
     this.load();
   }
@@ -38,6 +62,21 @@ export class AppComponent {
     if (ENV === 'development') {
       console.log('Deactivate Event', event);
     }
+  }
+
+  ngOnInit(): void {
+    /*this.http.get('https://unsplash.it/g/1920/1080/?random=true&blur=true')
+      .take(1)
+      .retry(3)
+      .subscribe((res: Response) => {
+        this.imageData = this.sanitizer.bypassSecurityTrustUrl(
+          URL.createObjectURL(new Blob([res.arrayBuffer()], { type: 'image/jpeg' }))
+        );
+        this.bgImageAnimationState = 'show';
+      });*/
+    setTimeout(() => {
+      this.bgImageAnimationState = 'show';
+    }, 5000)
   }
 
   private load(): void {
