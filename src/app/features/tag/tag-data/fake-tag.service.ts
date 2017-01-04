d6b41d724b81e32ece085ee8f244d06427422071
Clement Flodrops <clement.flodrops@gmail.com>
2017-01-04 23:12:27 +0100

    add fake tag service

---
+++ src/app/features/tag/tag-data/fake-tag.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/delay';
+
+import { Tag } from './tag.model';
+
+const TAGS: Tag[] = require('../../../../assets/mock-data/tags.json');
+
+@Injectable()
+export class FakeTagService {
+
+  getAllCategories(): Observable<Tag[]> {
+    return Observable.of(TAGS);
+  }
+}
--- src/app/features/tag/tag-data/tag-data.module.ts
+++ src/app/features/tag/tag-data/tag-data.module.ts
@@ -4,11 +4,13 @@ import { EffectsModule } from '@ngrx/effects';
 import { TagEffects } from './tag.effects';
 import { TagService } from './tag.service';
 import { TagActions } from './tag.actions';
+import { FakeTagService } from './fake-tag.service';
 
 @NgModule({
   imports: [ EffectsModule.run(TagEffects) ],
   providers: [
-    TagService,
+    // TagService,
+    { provide: TagService, useClass: FakeTagService },
     TagActions
   ]
 })
