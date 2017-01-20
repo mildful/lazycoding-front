961f2d86e77303ec00be5063bc95649b57144791
Clément FLODROPS <clement.flodrops@gmail.com>
2017-01-20 12:20:39 +0100

    close #12

--- src/app/features/share/share.component.ts
+++ src/app/features/share/share.component.ts
@@ -21,27 +21,56 @@ export class ShareComponent {
     this.uri = encodeURIComponent(document.URL);
   }
 
-  facebook(): void {
+  share(site: string): void {
+    try {
+      this.shareAPI();
+    } catch (e) {
+      console.warn(e.message);
+      console.warn('Move to traditional redirection.');
+      switch (site) {
+        case 'facebook': this.facebook(); break;
+        case 'facebook-like': this.facebookLike(); break;
+        case 'google': this.google(); break;
+        case 'linkedin': this.linkedin(); break;
+        case 'twitter': this.twitter(); break;
+        case 'link': this.link(); break;
+        default: console.warn(`Sharing to '${site}' is not available.`);
+      }
+    }
+  }
+
+  private facebook(): void {
     window.open(`https://www.facebook.com/sharer/sharer.php?u=${this.uri}&t=${this.title}`);
   }
 
-  facebookLike(): void {
+  private facebookLike(): void {
     window.open(`https://www.facebook.com/sharer/sharer.php?u=${this.uri}&t=${this.title}`);
   }
 
-  google(): void {
+  private google(): void {
     window.open(`https://plus.google.com/share?url=${this.uri}&title=${this.title}`);
   }
 
-  linkedin(): void {
+  private link(): void {
+
+  }
+
+  private linkedin(): void {
     window.open(`http://www.linkedin.com/shareArticle?mini=true&url=${this.uri}&title=${this.title}&summary=${this.description}&source=${this.origin}`);
   }
 
-  twitter(): void {
-    window.open(`https://twitter.com/intent/tweet?source=${this.uri}&text=${this.title}:${this.uri}&via=mildful`);
+  private shareAPI(): void {
+    if (navigator['share'] !== undefined) {
+      navigator['share']({
+        title: this.title,
+        text: this.description,
+        url: this.uri
+      }).then(() => console.log('Successful share'))
+        .catch(error => console.log('Error sharing:', error));
+    } else throw new Error('Share API is not available.');
   }
 
-  link(): void {
-
+  private twitter(): void {
+    window.open(`https://twitter.com/intent/tweet?source=${this.uri}&text=${this.title}:${this.uri}&via=mildful`);
   }
 }
